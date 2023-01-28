1ea2d608d9ee5aabf5ebbfa7c559b24f6c661f54
Ben <bboksner@LAPTOP-0U0LFUE0.localdomain>
2023-01-27 23:24:31 -0500

    removed react icons

--- src/ContactCard.jsx
+++ src/ContactCard.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
+import { FaUserCricle } from "@react-icons/all-files/fa/FaUserCircle";
 
-export default function ContactCard({url,fname="first",lname="last",phone=5555555555,relation}) {
+export default function ContactCard({url=<FaUserCircle></FaUserCircle>,fname="first",lname="last",phone=5555555555,relation}) {
   return (
     <div className='border-2'>
         <img src={url} alt="" />
         <p>{fname} {lname}</p>
         <a href="">{phone}</a>
+        
     </div>
   )
 }
