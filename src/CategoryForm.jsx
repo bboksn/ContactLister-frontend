4538de6681d8fec3edf67c4e7b0f4e722837f3d6
bboks <bboksner@gmail.com>
2023-02-17 17:29:55 -0500

    cat form wip

--- src/CategoryForm.jsx
+++ src/CategoryForm.jsx
@@ -2,11 +2,27 @@ import {React,useState} from 'react'
 
 export default function CategoryForm() {
  const [data,setData] = useState();
+ function fillData(e){
+  setData({...data,
+    [e.currentTarget.name]:e.currentTarget.value}
+    )
+ }
+ function handleData(e){
+  e.preventDefault()
+        fetch('http://localhost:9292/relationships',
+         {
+            method:"POST",
+            headers: {
+                'Content-Type': 'application/json',
+              },
+            body: JSON.stringify(data),
+        })
+ }
   return (
     <div className='flex-grow'>CategoryForm
-      <form action="" className='flex flex-col'>
-        <label>Category:</label> <input className='border-black border-2 rounded-md' type="text" />
-        <label>desc:</label> <input className='border-black border-2 rounded-md' type="text" />
+      <form onSubmit={handleData} className='flex flex-col'>
+        <label>Category:</label> <input onChange={fillData} className='border-black border-2 rounded-md' type="text" />
+        <label>desc:</label> <input onChange={fillData} className='border-black border-2 rounded-md' type="text" />
         <input type="submit" value="" />
       </form>
     </div>
